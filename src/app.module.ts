e77de4490439f31c73cf0eff8b88c8be699cf37d
James Zhang <zhjch05@live.com>
2022-07-19 16:35:22 -0700

    graphql and foo resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
+import { join } from 'path';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 import dbConfig from 'src/config/db.config';
+
+import FooResolver from './app.resolver';
 
 @Module({
   imports: [
@@ -18,6 +25,15 @@ import dbConfig from 'src/config/db.config';
       }),
       inject: [ConfigService],
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
   ],
+  providers: [FooResolver],
 })
 export default class AppModule {}
